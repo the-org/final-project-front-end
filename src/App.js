675a312f6e8098f3a5e1e46ecc11ed604d961308
Matt Burger <matt.burger1@outlook.com>
2019-05-14 10:40:16 -0700

    Update: removed connection test from render

--- src/App.js
+++ src/App.js
@@ -2,21 +2,18 @@ import React from 'react';
 import superagent from 'superagent';
 
 class App extends React.Component {
+  componentDidMount(){
+    superagent.get('https://final-back-end.herokuapp.com')
+      .then(res =>{
+        console.log(res);
+      });
+  }
   render() {
     return (
       <>
-        <Test />
+        <p>test</p>
       </>
     );
   }
 }
-
-class Test extends React.Component {
-  render() {
-    return(
-      await superagent.get('https://final-back-end.herokuapp.com/')
-    );
-  }
-}
-
 export default App;
