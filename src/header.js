76630e0b67e9fa84715349477a85c5619c780372
Matt Burger <42157655+mattburger@users.noreply.github.com>
2019-05-17 08:59:02 -0700

    Revert "Style"

--- src/header.js
+++ src/header.js
@@ -7,29 +7,22 @@ import Logout from './logout.js';
 const Header = (props) => (
   <Fragment>
     <nav>
-      <div id="menuToggle">
-        <input type="checkbox" />
-        <span></span>
-        <span></span>
-        <span></span>
-        <ul className="nav-links">
-          <div>devHub</div>
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/saved">Saved</NavLink>
-          <NavLink to="/about">About</NavLink>
-          {props.isLoggedIn ?
-            <Logout
-              handleUserLogout={props.handleUserLogout}
-              user={props.user}
-            /> :
-            <Login
-              handleUserInput={props.handleUserInput}
-              handleUserLogin={props.handleUserLogin}
-            />
-          }
-        </ul>
-      </div>
       <div className="brand">devHub</div>
+      <div className="nav-links">
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/saved">Saved</NavLink>
+        <NavLink to="/about">About</NavLink>
+        {props.isLoggedIn ?
+          <Logout 
+            handleUserLogout={props.handleUserLogout} 
+            user={props.user}
+          /> :
+          <Login
+            handleUserInput={props.handleUserInput}
+            handleUserLogin={props.handleUserLogin}
+          />
+        }
+      </div>
     </nav>
   </Fragment>
 );
